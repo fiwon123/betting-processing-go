f553ba4f1ef5ff4e2cd656d7f2fa91ab3909a15e
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-09-13 01:33:26 -0300

    fix migration using docker compose

--- scripts/migrations/run-migrate.sh
+++ scripts/migrations/run-migrate.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-exec /migrate -path ./migrations -database "postgres://${API_DB_USER}:${API_DB_PASSWORD}@db:${API_DB_PORT}/${API_DB}?sslmode=disable" up
+exec /migrate \
+    -path /migrations \
+    -database "postgres://${API_DB_USER}:${API_DB_PASSWORD}@${API_DB_HOST}:${API_DB_PORT}/${API_DB}?sslmode=disable" \
+    up
