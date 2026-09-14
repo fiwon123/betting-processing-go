e15cae14716ff58e9ff251f0d03ffeabb2e4bd56
Felipe Inoue <felipeinoue@yahoo.com.br>
2026-09-14 02:39:01 -0300

    update sqs-init and sqs

--- cmd/sqs-init/main.go
+++ cmd/sqs-init/main.go
@@ -2,53 +2,194 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
 
 	"github.com/fiwon123/betting-processing-go/internal/infra/cfg"
 )
 
-func main() {
-	region := cfg.GetEnv("AWS_REGION", "us-east-1")
-	endpoint := cfg.GetEnv(
-		"AWS_ENDPOINT_URL",
-		"http://ministack:4566",
-	)
-	key := cfg.GetEnv("AWS_ACCESS_KEY_ID", "test")
-	secret := cfg.GetEnv("AWS_SECRET_ACCESS_KEY", "test")
-	queueName := cfg.GetEnv("SQS_QUEUE_NAME", "local-queue")
+const (
+	defaultRegion    = "us-east-1"
+	defaultEndpoint  = "http://ministack:4566"
+	defaultAccessKey = "test"
+	defaultSecretKey = "test"
+	defaultQueueName = "wager-transactions.fifo"
+)
 
-	ctx := context.Background()
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	region := cfg.GetEnv("AWS_REGION", defaultRegion)
+	endpoint := cfg.GetEnv("AWS_ENDPOINT_URL", defaultEndpoint)
+	accessKey := cfg.GetEnv("AWS_ACCESS_KEY_ID", defaultAccessKey)
+	secretKey := cfg.GetEnv("AWS_SECRET_ACCESS_KEY", defaultSecretKey)
+
+	queueName := cfg.GetEnv("SQS_QUEUE_NAME", defaultQueueName)
+	outboundQueueName := cfg.GetEnv(
+		"SQS_OUTBOUND_QUEUE_NAME",
+		"wager-events-outbound.fifo",
+	)
+
+	if !strings.HasSuffix(queueName, ".fifo") {
+		log.Fatalf("SQS_QUEUE_NAME must end with .fifo: %s", queueName)
+	}
+
+	if !strings.HasSuffix(outboundQueueName, ".fifo") {
+		log.Fatalf(
+			"SQS_OUTBOUND_QUEUE_NAME must end with .fifo: %s",
+			outboundQueueName,
+		)
+	}
 
 	awsCfg, err := awsconfig.LoadDefaultConfig(
 		ctx,
 		awsconfig.WithRegion(region),
 		awsconfig.WithCredentialsProvider(
 			credentials.NewStaticCredentialsProvider(
-				key,
-				secret,
+				accessKey,
+				secretKey,
 				"",
 			),
 		),
 	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("load AWS config: %v", err)
 	}
 
 	client := sqs.NewFromConfig(awsCfg, func(options *sqs.Options) {
 		options.BaseEndpoint = aws.String(endpoint)
 	})
 
-	_, err = client.CreateQueue(ctx, &sqs.CreateQueueInput{
-		QueueName: aws.String(queueName),
-	})
+	mainQueueURL, err := createQueue(
+		ctx,
+		client,
+		queueName,
+		map[string]string{
+			"FifoQueue":                 "true",
+			"ContentBasedDeduplication": "true",
+			"VisibilityTimeout":         "30",
+			"MessageRetentionPeriod":    "1209600",
+		},
+	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("create main queue: %v", err)
 	}
 
-	log.Printf("SQS queue %s is ready", queueName)
+	log.Printf("SQS queue %s is ready: %s", queueName, mainQueueURL)
+
+	dlqName := strings.TrimSuffix(queueName, ".fifo") + "-dlq.fifo"
+
+	dlqURL, err := createQueue(
+		ctx,
+		client,
+		dlqName,
+		map[string]string{
+			"FifoQueue":                 "true",
+			"ContentBasedDeduplication": "true",
+			"MessageRetentionPeriod":    "1209600",
+		},
+	)
+	if err != nil {
+		log.Fatalf("create DLQ: %v", err)
+	}
+
+	log.Printf("SQS DLQ %s is ready: %s", dlqName, dlqURL)
+
+	outboundQueueURL, err := createQueue(
+		ctx,
+		client,
+		outboundQueueName,
+		map[string]string{
+			"FifoQueue":                 "true",
+			"ContentBasedDeduplication": "true",
+			"VisibilityTimeout":         "30",
+			"MessageRetentionPeriod":    "1209600",
+		},
+	)
+	if err != nil {
+		log.Fatalf("create outbound queue: %v", err)
+	}
+
+	log.Printf(
+		"SQS outbound queue %s is ready: %s",
+		outboundQueueName,
+		outboundQueueURL,
+	)
+
+	dlqARN, err := getQueueARN(ctx, client, dlqURL)
+	if err != nil {
+		log.Fatalf("get DLQ ARN: %v", err)
+	}
+
+	redrivePolicy, err := json.Marshal(map[string]string{
+		"deadLetterTargetArn": dlqARN,
+		"maxReceiveCount":     "3",
+	})
+	if err != nil {
+		log.Fatalf("encode redrive policy: %v", err)
+	}
+
+	_, err = client.SetQueueAttributes(ctx, &sqs.SetQueueAttributesInput{
+		QueueUrl: aws.String(mainQueueURL),
+		Attributes: map[string]string{
+			"RedrivePolicy": string(redrivePolicy),
+		},
+	})
+	if err != nil {
+		log.Fatalf("set redrive policy: %v", err)
+	}
+
+	log.Printf("Redrive policy set on %s", queueName)
+}
+
+func createQueue(
+	ctx context.Context,
+	client *sqs.Client,
+	queueName string,
+	attributes map[string]string,
+) (string, error) {
+	output, err := client.CreateQueue(ctx, &sqs.CreateQueueInput{
+		QueueName:  aws.String(queueName),
+		Attributes: attributes,
+	})
+	if err != nil {
+		return "", fmt.Errorf("create queue %q: %w", queueName, err)
+	}
+
+	if output.QueueUrl == nil || *output.QueueUrl == "" {
+		return "", fmt.Errorf("queue %q returned an empty URL", queueName)
+	}
+
+	return *output.QueueUrl, nil
+}
+
+func getQueueARN(
+	ctx context.Context,
+	client *sqs.Client,
+	queueURL string,
+) (string, error) {
+	output, err := client.GetQueueAttributes(ctx, &sqs.GetQueueAttributesInput{
+		QueueUrl:       aws.String(queueURL),
+		AttributeNames: []types.QueueAttributeName{types.QueueAttributeNameQueueArn},
+	})
+	if err != nil {
+		return "", fmt.Errorf("get queue attributes: %w", err)
+	}
+
+	queueARN := output.Attributes["QueueArn"]
+	if queueARN == "" {
+		return "", fmt.Errorf("QueueArn was not returned")
+	}
+
+	return queueARN, nil
 }
--- internal/infra/sqs/sqs.go
+++ internal/infra/sqs/sqs.go
@@ -3,10 +3,13 @@ package sqs
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	awsconfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
 	awssqs "github.com/aws/aws-sdk-go-v2/service/sqs"
+
 	"github.com/fiwon123/betting-processing-go/internal/infra/cfg"
 )
 
@@ -16,28 +19,52 @@ type Client struct {
 }
 
 func New(c cfg.Config) (*Client, error) {
-	if c.SQS.QueueURL == "" {
+	if strings.TrimSpace(c.SQS.QueueURL) == "" {
 		return nil, fmt.Errorf("SQS_QUEUE_URL is required")
+	}
+
+	if strings.TrimSpace(c.AWS.Region) == "" {
+		return nil, fmt.Errorf("AWS_REGION is required")
+	}
+
+	loadOptions := []func(*awsconfig.LoadOptions) error{
+		awsconfig.WithRegion(c.AWS.Region),
+	}
+
+	if c.AWS.Endpoint != "" {
+		accessKey := cfg.GetEnv("AWS_ACCESS_KEY_ID", "test")
+		secretKey := cfg.GetEnv("AWS_SECRET_ACCESS_KEY", "test")
+
+		loadOptions = append(
+			loadOptions,
+			awsconfig.WithCredentialsProvider(
+				credentials.NewStaticCredentialsProvider(
+					accessKey,
+					secretKey,
+					"",
+				),
+			),
+		)
 	}
 
 	awsCfg, err := awsconfig.LoadDefaultConfig(
 		context.Background(),
-		awsconfig.WithRegion(c.AWS.Region),
+		loadOptions...,
 	)
 	if err != nil {
 		return nil, fmt.Errorf("load AWS config: %w", err)
 	}
 
-	sqsOptions := []func(*awssqs.Options){}
+	clientOptions := []func(*awssqs.Options){}
 
 	if c.AWS.Endpoint != "" {
-		sqsOptions = append(sqsOptions, func(options *awssqs.Options) {
+		clientOptions = append(clientOptions, func(options *awssqs.Options) {
 			options.BaseEndpoint = aws.String(c.AWS.Endpoint)
 		})
 	}
 
 	return &Client{
-		client:   awssqs.NewFromConfig(awsCfg, sqsOptions...),
+		client:   awssqs.NewFromConfig(awsCfg, clientOptions...),
 		queueURL: c.SQS.QueueURL,
 	}, nil
 }
@@ -48,4 +75,79 @@ func (c *Client) GetQueueAttributes(
 	optFns ...func(*awssqs.Options),
 ) (*awssqs.GetQueueAttributesOutput, error) {
 	return c.client.GetQueueAttributes(ctx, params, optFns...)
+}
+
+func (c *Client) ReceiveMessage(
+	ctx context.Context,
+	params *awssqs.ReceiveMessageInput,
+	optFns ...func(*awssqs.Options),
+) (*awssqs.ReceiveMessageOutput, error) {
+	return c.client.ReceiveMessage(ctx, params, optFns...)
+}
+
+func (c *Client) DeleteMessage(
+	ctx context.Context,
+	params *awssqs.DeleteMessageInput,
+	optFns ...func(*awssqs.Options),
+) (*awssqs.DeleteMessageOutput, error) {
+	return c.client.DeleteMessage(ctx, params, optFns...)
+}
+
+func (c *Client) QueueURL() string {
+	return c.queueURL
+}
+
+func (c *Client) GetQueueURL(
+	ctx context.Context,
+	queueName string,
+) (string, error) {
+	output, err := c.client.GetQueueUrl(ctx, &awssqs.GetQueueUrlInput{
+		QueueName: aws.String(queueName),
+	})
+	if err != nil {
+		return "", fmt.Errorf("get queue URL for %q: %w", queueName, err)
+	}
+
+	if output.QueueUrl == nil || *output.QueueUrl == "" {
+		return "", fmt.Errorf("queue URL for %q was empty", queueName)
+	}
+
+	return *output.QueueUrl, nil
+}
+
+func (c *Client) SendMessage(
+	ctx context.Context,
+	queueURL string,
+	body string,
+	messageGroupID string,
+) error {
+	if strings.TrimSpace(queueURL) == "" {
+		return fmt.Errorf("queue URL is required")
+	}
+
+	if body == "" {
+		return fmt.Errorf("message body is required")
+	}
+
+	// FIFO queues require MessageGroupId.
+	if strings.HasSuffix(queueURL, ".fifo") &&
+		strings.TrimSpace(messageGroupID) == "" {
+		return fmt.Errorf("message group ID is required for FIFO queues")
+	}
+
+	input := &awssqs.SendMessageInput{
+		QueueUrl:    aws.String(queueURL),
+		MessageBody: aws.String(body),
+	}
+
+	if messageGroupID != "" {
+		input.MessageGroupId = aws.String(messageGroupID)
+	}
+
+	_, err := c.client.SendMessage(ctx, input)
+	if err != nil {
+		return fmt.Errorf("send SQS message: %w", err)
+	}
+
+	return nil
 }
